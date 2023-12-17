081f97363367640009d5a63749a43149ccf7b6c9
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-12-17 15:23:19 +0100

    Use DTO to prevent over-posting

--- Todo.cs
+++ Todo.cs
@@ -3,4 +3,6 @@ public class Todo
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    // not returned in the reponse body because using DTO 
+    public string? Secret { get; set; }
 }
