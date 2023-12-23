c9eadb24b505573e148f78672a0727ebb5cf9d14
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-12-23 15:47:35 +0100

    adds information such as the author, license, and description.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
 // app.Run();
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 // adds the database context to the dependency injection (DI)
@@ -16,7 +17,28 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 // app.MapGet("/", () => "Hello World!");
 builder.Services.AddEndpointsApiExplorer();
 // add swagger generator
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
 var app = builder.Build();
 
 // Enable the middleware for serving the generated JSON document and the Swagger UI
